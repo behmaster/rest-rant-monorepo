96e0479e853c57099dadbb804c4aaf8b7d65002f
Robbie Behm <behmaster@gmail.com>
2022-09-14 23:20:50 -0500

    role-based-authorization

--- backend/migrations/20220915034547-add-admin.js
+++ backend/migrations/20220915034547-add-admin.js
@@ -4,13 +4,13 @@ const bcrypt = require('bcrypt')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', [{
-      first_name: 'YOUR FIRST NAME',
-      last_name: 'YOUR LAST NAME',
-      email: 'admin@example.com',
-      role: 'admin',
-      password_digest: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
-      created_at: new Date(),
-      updated_at: new Date()
+        first_name: 'Robbie "admin"',
+        last_name: 'Behm',
+        email: 'admin@example.com',
+        role: 'admin',
+        password_digest: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
+        created_at: new Date(),
+        updated_at: new Date()
     }])
   },
 
--- backend/seeders/20220915034547-add-admin.js
+++ backend/seeders/20220915034547-add-admin.js
@@ -1,24 +1,22 @@
 'use strict';
+const bcrypt = require('bcrypt')
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [{
+      first_name: 'Robbie "admin"',
+      last_name: 'Behm',
+      email: 'admin@example.com',
+      role: 'admin',
+      password_digest: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', {
+      email: 'admin@example.com'
+    })
   }
-};
+}
